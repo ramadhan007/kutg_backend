3b46c391594af577903d2dee5e15ec17c3621cf0
ramadhan007 <ramadhanpangestu00@gmail.com>
2021-02-28 15:11:11 +0000

    update 3

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ShaynaAdmin - HTML5 Admin Template</title>
-    <meta name="description" content="ShaynaAdmin - HTML5 Admin Template">
+    <title>KUTG</title>
+    <meta name="description" content="KUTG">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 @stack('before-style')
 @include('includes.style')
